0cfbab8dce5ddaa3617617d3b707d2f0d95ef22b
artyrcha <artur.yamaliev@red-promo.ru>
2020-11-09 22:23:30 +0500

    commit 2

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-vgu66svxjco">
+      <div class="ldio-8qlwn30enp">
+        <div></div>
+        <div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
